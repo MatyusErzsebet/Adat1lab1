a8a026d2011a526ee4772a758f544e6977304131
Betty <matyus.betty@yahoo.com>
2021-02-19 15:16:28 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Adat1lab.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Adat1lab.exe"
+  "Adat1lab.exe.manifest"
+  "Adat1lab.pdb"
+  "CMakeFiles/Adat1lab.dir/main.c.obj"
+  "libAdat1lab.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Adat1lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void kiir(int);
+int main() {
+    int x=0, y[100][50]={0};
+    x++;
+    y[0][0]=1;
+    if(x<9){
+        x++;
+    }
+    kiir(x);
+    return 0;
+}
+void kiir(int x)
+{
+    x++;
+    printf( "%d", x);
+}
